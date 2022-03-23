da23559a2b06ed0035ca7af570fa00cb2f6f3f3d
dovedevic <dovedevic@gmail.com>
2022-03-23 15:59:06 -0400

    moved special data generators to the specific data generator module

--- src/generators/records.py
+++ src/generators/records.py
@@ -1,36 +1,7 @@
-import random
 import logging
 
 
-from src.generators.data import DataGenerator
-
-
-class UniformRandomDataGenerator(DataGenerator):
-    """Generate data using a uniform random data stream"""
-    def __init__(self, data_size_in_bytes: int, seed: int=None):
-        super().__init__(data_size_in_bytes)
-        self._seed = seed
-        if seed:
-            random.seed(self._seed)
-
-    def _generate(self):
-        return random.getrandbits(self._data_size_in_bytes * 8)
-
-
-class IncrementalDataGenerator(DataGenerator):
-    """Generate data by counting up from 0 by one, keep data size in mind for bit overflows"""
-    def _generate(self):
-        return self.items_generated % (2**(self._data_size_in_bytes*8))
-
-
-class ConstantDataGenerator(DataGenerator):
-    """Generate data using a constant value"""
-    def __init__(self, data_size_in_bytes: int, constant: int):
-        super().__init__(data_size_in_bytes)
-        self._constant = constant
-
-    def _generate(self):
-        return self._constant
+from src.generators.data import DataGenerator, IncrementalDataGenerator, UniformRandomDataGenerator
 
 
 class DatabaseRecordGenerator:
