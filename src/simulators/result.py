6cc472dbb9b5ab246899b41089c71880f7895719
dovedevic <dovedevic@gmail.com>
2022-03-30 19:52:00 -0400

    include runtime and lables, save states. added a simulation result to ensure query compliance

--- src/simulators/result.py
+++ src/simulators/result.py
@@ -1,38 +1,33 @@
-import enum
-
-from src.configuration import SystemConfiguration
-
-
-class ResultHistoryType(int, enum.Enum):
-    CYCLE = 0
-    TRA = 1
-    COPY = 2
-
-
-class ResultSet:
-    def __init__(self, config: SystemConfiguration):
-        self._config = config
-        self.history = list()
-        self.runtime = 0
+class RuntimeResult:
+    """Defines a list of simulation steps, runtimes, and actions"""
+    def __init__(self, runtime: int=0, label: str=None):
+        self.history = list() if runtime == 0 and label is None else [(runtime, label)]
+        self.runtime = runtime
 
     def __add__(self, other):
         if isinstance(other, self.__class__):
-            combined_result_set = ResultSet(self._config)
+            combined_result_set = RuntimeResult()
             combined_result_set.history = self.history + other.history
             combined_result_set.runtime = self.runtime + other.runtime
             return combined_result_set
         raise NotImplemented()
 
-    def _add_result(self, r_type: ResultHistoryType, time: float):
-        self.history.append((r_type, time))
-        self.runtime += time
-        return self
+    def step(self, runtime: int, label: str=None):
+        """Perform a simulation step, similar to adding two results"""
+        self.history.append((runtime, label))
+        self.runtime += runtime
 
-    def tra(self):
-        return self._add_result(ResultHistoryType.TRA, self._config.time_for_TRA_MAJ)
+    def save(self, path: str):
+        """Save the runtime result"""
+        with open(path, 'w') as fp:
+            fp.write(f"runtime: {self.runtime}ns\n")
+            fp.write(f"history: \n")
+            for runtime, label in self.history:
+                fp.write(f"\t{runtime}\t{label or ''}\n")
 
-    def copy(self):
-        return self._add_result(ResultHistoryType.COPY, self._config.time_for_AAP_rowclone)
 
-    def cycle(self, cycles=1):
-        return self._add_result(ResultHistoryType.CYCLE, self._config.time_per_blimp_cycle * cycles)
+class SimulationResult:
+    """Defines the return of the query. Returns the indexes of query-hit records"""
+    def __init__(self, result_record_indexes: [int]=list):
+        self.result_record_indexes = result_record_indexes
+        self.result_count = len(self.result_record_indexes)
