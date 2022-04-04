8fe932e635b7bfe7ae90f4c947c5c7d42c6c566b
dovedevic <dovedevic@gmail.com>
2022-04-04 04:04:57 -0400

    added simulation result helper to get index of bits in a byte array

--- src/simulators/result.py
+++ src/simulators/result.py
@@ -1,3 +1,6 @@
+import typing
+
+
 class RuntimeResult:
     """Defines a list of simulation steps, runtimes (in ns), and actions"""
     def __init__(self, runtime: float=0, label: str=None):
@@ -29,6 +32,32 @@ class RuntimeResult:
 
 class SimulationResult:
     """Defines the return of the query. Returns the indexes of query-hit records"""
-    def __init__(self, result_record_indexes: [int]=list):
+    def __init__(self, result_record_indexes: typing.List[int]=list):
         self.result_record_indexes = result_record_indexes
         self.result_count = len(self.result_record_indexes)
+
+    @staticmethod
+    def from_hitmap_byte_array(hitmap_byte_array: list, num_bits: int):
+        """Given a byte array, fetch all indexes that have a 1-bit"""
+        bitmaps_processed = 0
+        bit_indexes = []
+
+        # Iterate over all bytes
+        for byte in hitmap_byte_array:
+            # Have we exhausted all bitmaps in the hitmap?
+            if bitmaps_processed >= num_bits:
+                break
+
+            # Can this entire byte be processed?
+            if bitmaps_processed + 8 <= num_bits:
+                for b in range(8):
+                    if (1 << (7 - b)) & byte > 0:
+                        bit_indexes.append(bitmaps_processed)
+                    bitmaps_processed += 1
+            # Only a partial bit of the byte is processable
+            else:
+                for b in range(8 - (num_bits - bitmaps_processed)):
+                    if (1 << (7 - b)) & byte > 0:
+                        bit_indexes.append(bitmaps_processed)
+                    bitmaps_processed += 1
+        return SimulationResult(bit_indexes)
