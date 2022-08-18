0b17263837b7ec96e7b08917dbc8d80fe7a8a408
dovedevic <dovedevic@gmail.com>
2022-08-18 03:11:39 -0400

    added random/incr key constant data record generators

---
+++ src/generators/record_generators/assorted_key_const_data.py
@@ -0,0 +1,22 @@
+from generators import DatabaseRecordGenerator
+from generators.data_generators import ConstantDataGenerator, UniformRandomDataGenerator, IncrementalDataGenerator
+
+
+class RandomKeyConstantDataRecordGenerator(DatabaseRecordGenerator):
+    """
+    Record generator where the keys are uniformly random and the data is a constant
+    """
+    def __init__(self, pi_record_size: int, total_record_size: int, constant: int, total_records: int=None):
+        pidg = UniformRandomDataGenerator(pi_record_size)
+        cdg = ConstantDataGenerator(total_record_size - pi_record_size, constant)
+        super().__init__(pidg, cdg, total_records)
+
+
+class IncrementalKeyConstantDataRecordGenerator(DatabaseRecordGenerator):
+    """
+    Record generator where the keys are incremented and the data is a constant
+    """
+    def __init__(self, pi_record_size: int, total_record_size: int, constant: int, total_records: int=None):
+        pidg = IncrementalDataGenerator(pi_record_size)
+        cdg = ConstantDataGenerator(total_record_size - pi_record_size, constant)
+        super().__init__(pidg, cdg, total_records)
