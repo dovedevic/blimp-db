674c824ea66a72352e41394392749608d38f010a
dovedevic <dovedevic@gmail.com>
2022-08-25 18:33:29 -0400

    exposing child simulators

---
+++ src/simulators/__init__.py
@@ -0,0 +1,6 @@
+from .ambit import SimulatedAmbitBank
+from .blimp import SimulatedBlimpBank, SimulatedBlimpVBank
+from .blimp_ambit import SimulatedBlimpAmbitBank
+
+from .result import RuntimeResult, SimulationResult
+from .simulator import SimulatedBank
