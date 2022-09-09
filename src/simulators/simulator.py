8ee5334f49db6e3e4a57a8b0cf25ac47c9116713
dovedevic <dovedevic@gmail.com>
2022-09-09 06:49:09 -0400

    added helper sim method to perform n number of CPU cycles from the configuration

--- src/simulators/simulator.py
+++ src/simulators/simulator.py
@@ -1,6 +1,7 @@
 import logging
 
 from src.hardware.bank import Bank
+from src.simulators.result import RuntimeResult
 
 
 class SimulatedBank:
@@ -12,3 +13,15 @@ class SimulatedBank:
             ):
         self._logger = logger or logging.getLogger(self.__class__.__name__)
         self.bank_hardware = bank_hardware
+
+    def cpu_cycle(self, cycles=1, label="", return_labels=True) -> RuntimeResult:
+        """Perform a specified number of CPU cycles"""
+        if cycles <= 0:
+            raise ValueError("argument 'cycles' cannot be less than one")
+        runtime = RuntimeResult(
+            self.bank_hardware.hardware_configuration.time_per_cpu_cycle_ns,
+            label if return_labels else ""
+        )
+        for c in range(cycles - 1):
+            runtime.step(self.bank_hardware.hardware_configuration.time_per_cpu_cycle_ns)
+        return runtime
