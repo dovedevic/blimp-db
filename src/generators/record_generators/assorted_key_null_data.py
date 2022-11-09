b7160092d967cf275665660d2220c3d572f19087
dovedevic <dovedevic@gmail.com>
2022-11-09 14:50:33 -0500

    added BoundedRandomKeyNullDataRecordGenerator

--- src/generators/record_generators/assorted_key_null_data.py
+++ src/generators/record_generators/assorted_key_null_data.py
@@ -1,5 +1,6 @@
 from generators import DatabaseRecordGenerator
-from generators.data_generators import NullDataGenerator, UniformRandomDataGenerator, IncrementalDataGenerator
+from generators.data_generators import \
+    NullDataGenerator, UniformRandomDataGenerator, IncrementalDataGenerator, BoundedUniformRandomDataGenerator
 
 
 class RandomKeyNullDataRecordGenerator(DatabaseRecordGenerator):
@@ -18,5 +19,15 @@ class IncrementalKeyNullDataRecordGenerator(DatabaseRecordGenerator):
     """
     def __init__(self, pi_record_size: int, total_records: int=None):
         pidg = IncrementalDataGenerator(pi_record_size)
+        ddg = NullDataGenerator()
+        super().__init__(pidg, ddg, total_records)
+
+
+class BoundedRandomKeyNullDataRecordGenerator(DatabaseRecordGenerator):
+    """
+    Record generator where the keys are uniformly bounded random and the data is null
+    """
+    def __init__(self, pi_record_size: int, min_bound: int, max_bound: int, total_records: int=None):
+        pidg = BoundedUniformRandomDataGenerator(pi_record_size, min_bound, max_bound)
         ddg = NullDataGenerator()
         super().__init__(pidg, ddg, total_records)
