14aa851472717a2abe77e320e64351c7222f5759
dovedevic <dovedevic@gmail.com>
2023-03-21 23:49:11 -0400

    updated simple blimp hash table to use new general classes

--- studies/simple_query_benchmark/blimp_hash_table.py
+++ studies/simple_query_benchmark/blimp_hash_table.py
@@ -1,31 +1,49 @@
 from src.simulators.hashmap import *
 
 
-# Define 32bit KV pairs
-class HashTable32KV(GenericHashTableKV):
-    """Defines the pythonic structure of a 32-bit key 32-bit value object"""
-    _KEY_SIZE_BYTES = 4
-    _VALUE_SIZE_BYTES = 4
+# Define 32bit numbers
+class Object32bit(GenericHashTableValue):
+    _SIZE_BYTES = 4
 
 
-# Define a bucket using 4 byte count/next bucket values, with UINT32_MAX as the null value
-class BlimpHashTableBucket(GenericHashTableBucket[HashTable32KV]):
-    _NULL_VALUE = 2 ** 32 - 1
-    _ACTIVE_COUNT_SIZE_BYTES = 4
-    _NEXT_BUCKET_SIZE_BYTES = 4
+# Define 16bit numbers
+class Object16bit(GenericHashTableValue):
+    _SIZE_BYTES = 2
 
 
-# Generate a templated hash table that utilizes 32bit KVs and a simple hash; this is our custom table for BLIMP
-class BlimpSimpleHashTable(GenericHashMapObject[HashTable32KV, BlimpHashTableBucket]):
-    bucket_capacity = 15
+# Define 16bit numbers that have a null value as the max int
+class MaxIntNull16bObject(Object16bit):
+    _NULL_VALUE = 2 ** (Object16bit._SIZE_BYTES * 8) - 1
 
-    def __init__(self, initial_buckets: int, maximum_buckets: int):
+
+# Define a null payload
+class NullPayload(GenericHashTableValuePayload):
+    _PAYLOAD_OBJECTS = []
+
+
+# Define 32bit key, null payload objects
+class Hash32bitObjectNullPayload(GenericHashTableObject):
+    _KEY_OBJECT = Object32bit
+    _PAYLOAD_OBJECT = NullPayload
+
+
+# Define a bucket as a collection of 31 Hash32bitObjectNullPayload, with 16bit metadata
+class BlimpBucket(GenericHashTableBucket):
+    _KEY_PAYLOAD_OBJECT = Hash32bitObjectNullPayload
+    _BUCKET_OBJECT_CAPACITY = 31
+    _META_NEXT_BUCKET_OBJECT = MaxIntNull16bObject
+    _META_ACTIVE_COUNT_OBJECT = Object16bit
+
+
+# Define a hash set that uses BlimpBuckets
+class BlimpHashSet(GenericHashMap):
+    _BUCKET_OBJECT = BlimpBucket
+
+    def __init__(self, initial_buckets: int, maximum_buckets: int, **kwargs):
         super().__init__(
             initial_buckets,
             maximum_buckets,
-            bucket_capacity=self.bucket_capacity,
-            kv_generator=HashTable32KV,
-            bucket_generator=BlimpHashTableBucket
+            **kwargs
         )
 
         assert (initial_buckets & (initial_buckets - 1) == 0) and initial_buckets != 0, \
