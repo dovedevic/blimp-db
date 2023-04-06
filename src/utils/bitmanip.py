b3eb938a54ee71c4b9dd291eb0f1375f1f63e913
dovedevic <dovedevic@gmail.com>
2023-04-05 23:27:32 -0400

    added return typing

--- src/utils/bitmanip.py
+++ src/utils/bitmanip.py
@@ -1,14 +1,17 @@
-def lsb_bit(value: int, least_significant_bit_index: int):
+from typing import List
+
+
+def lsb_bit(value: int, least_significant_bit_index: int) -> int:
     """Get a bit from the provided value at the specified lsb index (index 0 is lsb)"""
     return value & (1 << least_significant_bit_index) >> least_significant_bit_index
 
 
-def msb_bit(value: int, most_significant_bit_index: int, bit_width: int):
+def msb_bit(value: int, most_significant_bit_index: int, bit_width: int) -> int:
     """Get a bit from the provided value at the specified msb index (index 0 is msb)"""
     return ((((2**bit_width) - 1) >> most_significant_bit_index) & value) >> (bit_width - most_significant_bit_index - 1)
 
 
-def int_to_byte_array(value: int, value_byte_size: int) -> [int]:
+def int_to_byte_array(value: int, value_byte_size: int) -> List[int]:
     """Given a value, deconstruct it to a byte array with the size specified"""
     byte_array = []
     # Construct a byte array byte-by-byte
