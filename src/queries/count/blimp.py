bd85d68b12965c605086a52bd8ae4600ba26ba02
dovedevic <dovedevic@gmail.com>
2023-04-06 17:30:22 -0400

    updating args to use kwargs due to alu args update

--- src/queries/count/blimp.py
+++ src/queries/count/blimp.py
@@ -9,7 +9,7 @@ from src.utils.bitmanip import byte_array_to_int
 from src.simulators.blimp import SimulatedBlimpBank
 
 
-class _BlimpHitmapCount(
+class BlimpHitmapCount(
     Query[
         SimulatedBlimpBank,
         DataLayoutConfiguration[
@@ -18,7 +18,7 @@ class _BlimpHitmapCount(
         ]
     ]
 ):
-    def _perform_operation(
+    def perform_operation(
             self,
             hitmap_index: int,
             return_labels: bool=False,
@@ -42,18 +42,30 @@ class _BlimpHitmapCount(
         hitmap_base = self.layout_configuration.row_mapping.hitmaps[0] + rows_per_hitmap * hitmap_index
 
         # Begin by enabling BLIMP
-        runtime = self.simulator.blimp_begin(return_labels)
+        runtime = self.simulator.blimp_begin(return_labels=return_labels)
         count = 0
 
         # Iterate over all hitmap rows
-        runtime += self.simulator.blimp_cycle(3, "; loop start", return_labels)
+        runtime += self.simulator.blimp_cycle(
+            cycles=3,
+            label="; loop start",
+            return_labels=return_labels
+        )
         for h in range(rows_per_hitmap):
-            runtime += self.simulator.blimp_cycle(1, "; hitmap row calculation", return_labels)
+            runtime += self.simulator.blimp_cycle(
+                cycles=1,
+                label="; hitmap row calculation",
+                return_labels=return_labels
+            )
             # Calculate the hitmap we are targeting
             hitmap_row = hitmap_base + h
 
             # move hitmap[index] into v1
-            runtime += self.simulator.blimp_load_register(self.simulator.blimp_v1, hitmap_row, return_labels)
+            runtime += self.simulator.blimp_load_register(
+                register=self.simulator.blimp_v1,
+                row=hitmap_row,
+                return_labels=return_labels
+            )
 
             # count the number of bits set, use pop count if the hardware is present, otherwise use software
             if self.layout_configuration.hardware_configuration.blimp_extension_popcount:
@@ -61,44 +73,34 @@ class _BlimpHitmapCount(
             else:
                 result_method = self.simulator.blimp_bit_count
             runtime += result_method(
-                self.simulator.blimp_v1,
-                0,
-                self.hardware.hardware_configuration.row_buffer_size_bytes,
-                self.hardware.hardware_configuration.blimp_processor_bit_architecture // 8,
-                return_labels
+                register=self.simulator.blimp_v1,
+                start_index=0,
+                end_index=self.hardware.hardware_configuration.row_buffer_size_bytes,
+                element_width=self.hardware.hardware_configuration.blimp_processor_bit_architecture // 8,
+                return_labels=return_labels
             )
 
             # update our count
-            runtime += self.simulator.blimp_cycle(1, "; count update", return_labels)
+            runtime += self.simulator.blimp_cycle(
+                cycles=1,
+                label="; count update",
+                return_labels=return_labels
+            )
             count += byte_array_to_int(
                 self.simulator.registers[self.simulator.blimp_v1]
                 [0: self.hardware.hardware_configuration.blimp_processor_bit_architecture // 8]
             )
 
-            runtime += self.simulator.blimp_cycle(2, "; loop return", return_labels)
-        runtime += self.simulator.blimp_end(return_labels)
+            runtime += self.simulator.blimp_cycle(
+                cycles=2,
+                label="; loop return",
+                return_labels=return_labels
+            )
+
+        runtime += self.simulator.blimp_end(return_labels=return_labels)
 
         # We have finished the query, return a dummy result
 
         result = HitmapResult([])
         result.result_count = count
         return runtime, result
-
-
-class BlimpHitmapCount(_BlimpHitmapCount):
-    def perform_operation(
-            self,
-            hitmap_index: int,
-            return_labels: bool=False
-    ) -> (RuntimeResult, HitmapResult):
-        """
-        Perform a BLIMP hitmap count operation on a provided hitmap index.
-
-        @param hitmap_index: Which hitmap to target for bit counting
-        @param return_labels: Whether to return debug labels with the RuntimeResult history
-
-        """
-        return self._perform_operation(
-            hitmap_index=hitmap_index,
-            return_labels=return_labels,
-        )
