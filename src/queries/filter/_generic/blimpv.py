fb6869b7fb1302711991ac061c201f7dc3ab67f7
dovedevic <dovedevic@gmail.com>
2023-04-06 20:22:52 -0400

    updated simulator alu kwargs

--- src/queries/filter/_generic/blimpv.py
+++ src/queries/filter/_generic/blimpv.py
@@ -5,8 +5,7 @@ from src.queries.filter._generic.operations import GenericArithmeticLogicalOpera
 from src.simulators.result import RuntimeResult, HitmapResult
 from src.data_layout_mappings.architectures import \
     BlimpIndexHitmapBankLayoutConfiguration
-
-from src.simulators import SimulatedBlimpVBank
+from src.simulators.hardware import SimulatedBlimpVBank
 
 
 class _BlimpVHitmapGenericScalarALO(
@@ -61,136 +60,177 @@ class _BlimpVHitmapGenericScalarALO(
         base_data_row = self.layout_configuration.row_mapping.data[0]
 
         # Begin by enabling BLIMP
-        runtime = self.simulator.blimp_begin(return_labels)
+        runtime = self.simulator.blimp_begin(return_labels=return_labels)
 
         # Clear a register for temporary hitmaps
-        runtime += self.simulator.blimpv_set_register_to_zero(self.simulator.blimp_v2, return_labels)
+        runtime += self.simulator.blimpv_set_register_to_zero(
+            register=self.simulator.blimp_v2,
+            return_labels=return_labels
+        )
 
         # Iterate over all data rows
-        runtime += self.simulator.blimp_cycle(3, "; loop start", return_labels)
+        runtime += self.simulator.blimp_cycle(
+            cycles=3,
+            label="; loop start",
+            return_labels=return_labels
+        )
         for d in range(self.layout_configuration.row_mapping.data[1]):
 
-            runtime += self.simulator.blimp_cycle(1, "; data row calculation", return_labels)
+            runtime += self.simulator.blimp_cycle(
+                cycles=1,
+                label="; data row calculation",
+                return_labels=return_labels
+            )
             data_row = base_data_row + d
 
             # Load in elements_per_row elements into the vector register
-            runtime += self.simulator.blimp_load_register(self.simulator.blimp_v1, data_row, return_labels)
+            runtime += self.simulator.blimp_load_register(
+                register=self.simulator.blimp_v1,
+                row=data_row,
+                return_labels=return_labels
+            )
 
             # Perform the operation
             if operation == GenericArithmeticLogicalOperation.EQ:
                 runtime += self.simulator.blimpv_alu_int_eq_val(
-                    self.simulator.blimp_v1,
-                    pi_element_size_bytes,
-                    pi_element_size_bytes,
-                    value,
-                    return_labels
+                    register_a=self.simulator.blimp_v1,
+                    sew=pi_element_size_bytes,
+                    stride=pi_element_size_bytes,
+                    value=value,
+                    return_labels=return_labels
                 )
             elif operation == GenericArithmeticLogicalOperation.LT:
                 runtime += self.simulator.blimpv_alu_int_lt_val(
-                    self.simulator.blimp_v1,
-                    pi_element_size_bytes,
-                    pi_element_size_bytes,
-                    value,
-                    return_labels
+                    register_a=self.simulator.blimp_v1,
+                    sew=pi_element_size_bytes,
+                    stride=pi_element_size_bytes,
+                    value=value,
+                    return_labels=return_labels
                 )
             elif operation == GenericArithmeticLogicalOperation.GT:
                 runtime += self.simulator.blimpv_alu_int_gt_val(
-                    self.simulator.blimp_v1,
-                    pi_element_size_bytes,
-                    pi_element_size_bytes,
-                    value,
-                    return_labels
+                    register_a=self.simulator.blimp_v1,
+                    sew=pi_element_size_bytes,
+                    stride=pi_element_size_bytes,
+                    value=value,
+                    return_labels=return_labels
                 )
             elif operation == GenericArithmeticLogicalOperation.GTE:
                 runtime += self.simulator.blimpv_alu_int_gte_val(
-                    self.simulator.blimp_v1,
-                    pi_element_size_bytes,
-                    pi_element_size_bytes,
-                    value,
-                    return_labels
+                    register_a=self.simulator.blimp_v1,
+                    sew=pi_element_size_bytes,
+                    stride=pi_element_size_bytes,
+                    value=value,
+                    return_labels=return_labels
                 )
             elif operation == GenericArithmeticLogicalOperation.LTE:
                 runtime += self.simulator.blimpv_alu_int_lte_val(
-                    self.simulator.blimp_v1,
-                    pi_element_size_bytes,
-                    pi_element_size_bytes,
-                    value,
-                    return_labels
+                    register_a=self.simulator.blimp_v1,
+                    sew=pi_element_size_bytes,
+                    stride=pi_element_size_bytes,
+                    value=value,
+                    return_labels=return_labels
                 )
 
             # Coalesce the bitmap
             runtime += self.simulator.blimpv_coalesce_register_hitmap(
-                self.simulator.blimp_v1,
-                pi_element_size_bytes,
-                pi_element_size_bytes,
-                elements_processed % (self.hardware.hardware_configuration.row_buffer_size_bytes * 8),
-                return_labels
+                register_a=self.simulator.blimp_v1,
+                sew=pi_element_size_bytes,
+                stride=pi_element_size_bytes,
+                bit_offset=elements_processed % (self.hardware.hardware_configuration.row_buffer_size_bytes * 8),
+                return_labels=return_labels
             )
 
             # Or the bitmap into the temporary one
             runtime += self.simulator.blimpv_alu_int_or(
-                self.simulator.blimp_v1,
-                self.simulator.blimp_v2,
-                self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
-                self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
-                return_labels
+                register_a=self.simulator.blimp_v1,
+                register_b=self.simulator.blimp_v2,
+                sew=self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
+                stride=self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
+                return_labels=return_labels
             )
 
-            runtime += self.simulator.blimp_cycle(1, "; metadata calculation", return_labels)
+            runtime += self.simulator.blimp_cycle(
+                cycles=1,
+                label="; metadata calculation",
+                return_labels=return_labels
+            )
             elements_processed = min(
                 elements_processed + elements_per_row,
                 self.layout_configuration.layout_metadata.total_records_processable
             )
 
             # do we need to reset?
-            runtime += self.simulator.blimp_cycle(3, "; cmp elements processed", return_labels)
+            runtime += self.simulator.blimp_cycle(
+                cycles=2,
+                label="; cmp elements processed",
+                return_labels=return_labels
+            )
             if elements_processed % (self.hardware.hardware_configuration.row_buffer_size_bytes * 8) == 0:
 
-                runtime += self.simulator.blimp_cycle(3, "; cmp negate", return_labels)
+                runtime += self.simulator.blimp_cycle(
+                    cycles=1,
+                    label="; cmp negate",
+                    return_labels=return_labels
+                )
                 if negate:
                     # If we are negating, invert v2 (since it was just saved)
                     runtime += self.simulator.blimpv_alu_int_not(
-                        self.simulator.blimp_v2,
-                        self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
-                        self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
-                        return_labels
+                        register_a=self.simulator.blimp_v2,
+                        sew=self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
+                        stride=self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
+                        return_labels=return_labels
                     )
 
                 # Save the hitmap
                 runtime += self.simulator.blimp_save_register(
-                    self.simulator.blimp_v2,
-                    hitmap_base +
-                    (elements_processed // (self.hardware.hardware_configuration.row_buffer_size_bytes * 8))
-                    - 1,
-                    return_labels
+                    register=self.simulator.blimp_v2,
+                    row=hitmap_base +
+                    (elements_processed // (self.hardware.hardware_configuration.row_buffer_size_bytes * 8)) - 1,
+                    return_labels=return_labels
                 )
 
                 # Reset to save a new one
-                runtime += self.simulator.blimpv_set_register_to_zero(self.simulator.blimp_v2, return_labels)
+                runtime += self.simulator.blimpv_set_register_to_zero(
+                    register=self.simulator.blimp_v2,
+                    return_labels=return_labels
+                )
 
-            runtime += self.simulator.blimp_cycle(2, "; loop return", return_labels)
+            runtime += self.simulator.blimp_cycle(
+                cycles=2,
+                label="; loop return",
+                return_labels=return_labels
+            )
 
         # were done with records processing, but we need to save one last time possibly
-        runtime += self.simulator.blimp_cycle(3, "; cmp save", return_labels)
+        runtime += self.simulator.blimp_cycle(
+            cycles=2,
+            label="; cmp save",
+            return_labels=return_labels
+        )
         if elements_processed % (self.hardware.hardware_configuration.row_buffer_size_bytes * 8) != 0:
-            runtime += self.simulator.blimp_cycle(3, "; cmp negate", return_labels)
+            runtime += self.simulator.blimp_cycle(
+                cycles=1,
+                label="; cmp negate",
+                return_labels=return_labels
+            )
             if negate:
                 # If we are negating, invert v2 (since it was just saved)
                 runtime += self.simulator.blimpv_alu_int_not(
-                    self.simulator.blimp_v2,
-                    self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
-                    self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
-                    return_labels
+                    register_a=self.simulator.blimp_v2,
+                    sew=self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
+                    stride=self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
+                    return_labels=return_labels
                 )
 
             runtime += self.simulator.blimp_save_register(
-                self.simulator.blimp_v2,
-                hitmap_base +
+                register=self.simulator.blimp_v2,
+                row=hitmap_base +
                 (elements_processed // (self.hardware.hardware_configuration.row_buffer_size_bytes * 8)),
-                return_labels
+                return_labels=return_labels
             )
 
-        runtime += self.simulator.blimp_end(return_labels)
+        runtime += self.simulator.blimp_end(return_labels=return_labels)
 
         # Do we need to pad off remaining hits? This will be handled already by us with V-ASM but we need to do it here
         remainder = elements_processed % (self.hardware.hardware_configuration.row_buffer_size_bytes * 8)
@@ -217,7 +257,7 @@ class _BlimpVHitmapGenericScalarALO(
             hitmap_byte_array += self.simulator.bank_hardware.get_row_bytes(hitmap_row)
 
         result = HitmapResult.from_hitmap_byte_array(
-            hitmap_byte_array,
-            self.layout_configuration.layout_metadata.total_records_processable
+            hitmap_byte_array=hitmap_byte_array,
+            num_bits=self.layout_configuration.layout_metadata.total_records_processable
         )
         return runtime, result
