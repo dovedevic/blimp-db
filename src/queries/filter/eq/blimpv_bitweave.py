c21891ebf2b3961954f7c0186d2ebd44ce2125ff
dovedevic <dovedevic@gmail.com>
2023-04-06 21:37:21 -0400

    fixed mask/label arg issue, updated timings

--- src/queries/filter/eq/blimpv_bitweave.py
+++ src/queries/filter/eq/blimpv_bitweave.py
@@ -5,8 +5,7 @@ from src.simulators.result import RuntimeResult, HitmapResult
 from src.utils import bitmanip
 from src.data_layout_mappings.architectures import \
     BlimpHitmapIndexBitweaveBankLayoutConfiguration
-
-from src.simulators import SimulatedBlimpVBank
+from src.simulators.hardware import SimulatedBlimpVBank
 
 
 class _BlimpVBitweaveHitmapEquality(
@@ -57,88 +56,141 @@ class _BlimpVBitweaveHitmapEquality(
         hitmap_base = self.layout_configuration.row_mapping.hitmaps[0] + rows_per_hitmap * hitmap_index
 
         # Begin by enabling BLIMP
-        runtime = self.simulator.blimp_begin(return_labels)
+        runtime = self.simulator.blimp_begin(return_labels=return_labels)
 
         # Iterate over all hitmap rows
-        runtime += self.simulator.blimp_cycle(3, "; loop start", return_labels)
+        runtime += self.simulator.blimp_cycle(
+            cycles=3,
+            label="; loop start",
+            return_labels=return_labels
+        )
         for h in range(rows_per_hitmap):
-            runtime += self.simulator.blimp_cycle(1, "; hitmap row calculation", return_labels)
+            runtime += self.simulator.blimp_cycle(
+                cycles=1,
+                label="; hitmap row calculation",
+                return_labels=return_labels
+            )
             # Calculate the hitmap we are targeting: Base Hitmap address + hitmap index + sub-hitmap index
             hitmap_row = hitmap_base + h
 
             # Iterate over the bits per this chunk of records
 
             # Optimization
-            runtime += self.simulator.blimp_cycle(8, "; pre-row calculation", return_labels)
+            runtime += self.simulator.blimp_cycle(
+                cycles=8,
+                label="; pre-row calculation",
+                return_labels=return_labels
+            )
             base_row_to_check = self.layout_configuration.row_mapping.data[0] + \
                 h * self.layout_configuration.database_configuration.total_index_size_bytes * 8 + \
                 pi_subindex_offset_bytes * 8
 
             # Initialize V2 for a temporary hitmap
-            runtime += self.simulator.blimp_load_register(self.simulator.blimp_v2, hitmap_row, return_labels)
+            runtime += self.simulator.blimp_load_register(
+                register=self.simulator.blimp_v2,
+                row=hitmap_row,
+                return_labels=return_labels
+            )
 
-            runtime += self.simulator.blimp_cycle(3, "; inner loop start", return_labels)
+            runtime += self.simulator.blimp_cycle(
+                cycles=3,
+                label="; inner loop start",
+                return_labels=return_labels
+            )
             for b in range(pi_element_size_bytes * 8):
-                runtime += self.simulator.blimp_cycle(5, "; bit calculation", return_labels)
+                runtime += self.simulator.blimp_cycle(
+                    cycles=2,
+                    label="; bit calculation",
+                    return_labels=return_labels
+                )
                 bit_at_value = bitmanip.msb_bit(value, b, 8 * pi_element_size_bytes)
 
                 # Calculate the row offset to fetch
                 # PI/Key base row + record chunk index + subindex offset + bit
-                runtime += self.simulator.blimp_cycle(1, "; row calculation", return_labels)
+                runtime += self.simulator.blimp_cycle(
+                    cycles=1,
+                    label="; row calculation",
+                    return_labels=return_labels
+                )
                 row_to_check = base_row_to_check + b
 
                 # TO perform EQUAL we want to do PI[bit] XNOR value[bit]
 
                 # let v1 be PI[bit]
-                runtime += self.simulator.blimp_load_register(self.simulator.blimp_v1, row_to_check, return_labels)
+                runtime += self.simulator.blimp_load_register(
+                    register=self.simulator.blimp_v1,
+                    row=row_to_check,
+                    return_labels=return_labels
+                )
 
                 # let v1 now be PI[bit] XNOR value[bit]
-                runtime += self.simulator.blimp_cycle(3, "; cmp bit", return_labels)
+                runtime += self.simulator.blimp_cycle(
+                    cycles=2,
+                    label="cmp bit",
+                    return_labels=return_labels
+                )
                 if bit_at_value:
                     runtime += self.simulator.blimpv_alu_int_xnor_val(
-                        self.simulator.blimp_v1,
-                        self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
-                        self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
-                        2**(self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes*8) - 1,
-                        return_labels
+                        register_a=self.simulator.blimp_v1,
+                        sew=self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
+                        stride=self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
+                        value=2**(self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes*8) - 1,
+                        return_labels=return_labels
                     )
                 else:
                     runtime += self.simulator.blimpv_alu_int_xnor_val(
-                        self.simulator.blimp_v1,
-                        self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
-                        self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
-                        0,
-                        return_labels
+                        register_a=self.simulator.blimp_v1,
+                        sew=self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
+                        stride=self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
+                        value=0,
+                        return_labels=return_labels
                     )
 
                 # AND the result into the existing hitmap (v1 AND v2), v2 has the result
                 runtime += self.simulator.blimpv_alu_int_and(
-                    self.simulator.blimp_v1,
-                    self.simulator.blimp_v2,
-                    self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
-                    self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
-                    return_labels
+                    register_a=self.simulator.blimp_v1,
+                    register_b=self.simulator.blimp_v2,
+                    sew=self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
+                    stride=self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
+                    return_labels=return_labels
                 )
 
-                runtime += self.simulator.blimp_cycle(2, "; inner loop return", return_labels)
+                runtime += self.simulator.blimp_cycle(
+                    cycles=2,
+                    label="; inner loop return",
+                    return_labels=return_labels
+                )
 
             # At this point, all bits for this chunk of records is operated on, thus completing a hitmap row calculation
             # Check if this operation requires the hitmap result to be inverted (NOT EQUAL vs EQUAL)
-            runtime += self.simulator.blimp_cycle(3, "; cmp negate", return_labels)
+            runtime += self.simulator.blimp_cycle(
+                cycles=1,
+                label="cmp negate",
+                return_labels=return_labels
+            )
             if negate:
                 # If we are negating, invert v2 (since it was just saved)
                 runtime += self.simulator.blimpv_alu_int_not(
-                    self.simulator.blimp_v2,
-                    self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
-                    self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
-                    return_labels
+                    register_a=self.simulator.blimp_v2,
+                    sew=self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
+                    stride=self.layout_configuration.hardware_configuration.blimpv_sew_max_bytes,
+                    return_labels=return_labels
                 )
 
             # Save the row back into the bank
-            runtime += self.simulator.blimp_save_register(self.simulator.blimp_v2, hitmap_row, return_labels)
+            runtime += self.simulator.blimp_save_register(
+                register=self.simulator.blimp_v2,
+                row=hitmap_row,
+                return_labels=return_labels
+            )
 
-            runtime += self.simulator.blimp_cycle(2, "; loop return", return_labels)
-        runtime += self.simulator.blimp_end(return_labels)
+            runtime += self.simulator.blimp_cycle(
+                cycles=2,
+                label="; outer loop return",
+                return_labels=return_labels
+            )
+
+        runtime += self.simulator.blimp_end(return_labels=return_labels)
 
         # We have finished the query, fetch the hitmap to one single hitmap row
         hitmap_byte_array = []
@@ -150,8 +202,8 @@ class _BlimpVBitweaveHitmapEquality(
             hitmap_byte_array += self.simulator.bank_hardware.get_row_bytes(hitmap_row)
 
         result = HitmapResult.from_hitmap_byte_array(
-            hitmap_byte_array,
-            self.layout_configuration.layout_metadata.total_records_processable
+            hitmap_byte_array=hitmap_byte_array,
+            num_bits=self.layout_configuration.layout_metadata.total_records_processable
         )
         return runtime, result
 
