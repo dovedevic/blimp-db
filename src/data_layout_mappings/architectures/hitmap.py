770108998d3ffc6dd41eb393233f474ed7f67ee4
dovedevic <dovedevic@gmail.com>
2023-04-20 21:09:47 -0400

    implemented method to insert a hitmap result into a provided bank

--- src/data_layout_mappings/architectures/hitmap.py
+++ src/data_layout_mappings/architectures/hitmap.py
@@ -1,3 +1,5 @@
+import math
+
 from pydantic import Field
 
 from src.data_layout_mappings.methods import place_hitmap
@@ -23,7 +25,23 @@ class GenericHitmapBankLayoutConfiguration(DataLayoutConfiguration):
         )
 
     def load_hitmap_result(self, bank: Bank, result: HitmapResult, index: int):
-        pass
+        rb_bits = self.hardware_configuration.row_buffer_size_bytes * 8
+        rows_per_hitmap = self._layout_metadata.total_rows_for_hitmaps // self.database_configuration.hitmap_count
+        result_index = 0
+
+        for hitmap_index in range(math.ceil(result.max_bits / rb_bits)):
+            stop_value = rb_bits * (hitmap_index + 1) + 1
+            hitmap_value = 0
+
+            while result_index < len(result.result_record_indexes) and \
+                    result.result_record_indexes[result_index] < stop_value:
+                hitmap_value += 1 << ((rb_bits - 1) - result.result_record_indexes[result_index] % rb_bits)
+                result_index += 1
+
+            bank.set_raw_row(
+                row_index=self._row_mapping_set.hitmaps[0] + index * rows_per_hitmap + hitmap_index,
+                value=hitmap_value
+            )
 
 
 class GenericHitmapRowMapping(RowMappingSet):
