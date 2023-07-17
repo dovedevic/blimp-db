b2d7813a6a3d6373b09f7016bb9945b8ee5082b6
dovedevic <dovedevic@gmail.com>
2023-07-17 02:01:34 -0400

    refactored db class to be db and data gen compliant, added params for internal functionality tuning

--- studies/simple_query_benchmark/database.py
+++ studies/simple_query_benchmark/database.py
@@ -1,13 +1,53 @@
 import random
 
+from src.generators import DatabaseRecordGenerator, DataGenerator
+from src.generators.data_generators import NullDataGenerator
+
 
 class SQBDatabase:
-    def __init__(self, n_a, n_b):
-        random.seed(0)  # Initialize the random generator with a seed to ensure reproducibility.
-        self._a = [(i, random.randint(0, n_b - 1), random.randint(0, 9), random.randint(0, 99)) for i in range(n_a)]
-        self._b = [(i, random.randint(0, 9), random.randint(0, 99)) for i in range(n_b)]
-        random.shuffle(self._a)
-        random.shuffle(self._b)
+    def __init__(
+            self,
+            table_a_size: int,
+            table_b_size: int,
+            shuffle_indices: bool=False,
+            seed: int=0,
+            index_size_bytes: int=4
+    ):
+        random.seed(seed)  # Initialize the random generator with a seed to ensure reproducibility.
+        self._a = [
+            (i, random.randint(0, table_b_size - 1), random.randint(0, 9), random.randint(0, 99))
+            for i in range(table_a_size)
+        ]
+        self._b = [
+            (i, random.randint(0, 9), random.randint(0, 99))
+            for i in range(table_b_size)
+        ]
+        self.index_size_bytes = index_size_bytes
+
+        if shuffle_indices:
+            random.shuffle(self._a)
+            random.shuffle(self._b)
+
+        class SQBColumnDataGenerator(DataGenerator):
+            def __init__(self, data_link, data_index, index_size):
+                self.data_link = data_link
+                self.data_index = data_index
+                super().__init__(index_size)
+
+            def _generate(self):
+                return self.data_link[self.items_generated][self.data_index]
+
+        class SQBDatabaseRecordGenerator(DatabaseRecordGenerator):
+            def __init__(self, data_link, data_index, index_size, total_records):
+                super().__init__(
+                    pi_generator=SQBColumnDataGenerator(data_link, data_index, index_size),
+                    data_generator=NullDataGenerator(),
+                    total_records=total_records
+                )
+
+        self.a_k_generator = SQBDatabaseRecordGenerator(self._a, 0, index_size_bytes, len(self._a))
+        self.a_10_generator = SQBDatabaseRecordGenerator(self._a, 2, index_size_bytes, len(self._a))
+        self.a_100_generator = SQBDatabaseRecordGenerator(self._a, 3, index_size_bytes, len(self._a))
 
     @property
     def a(self):
